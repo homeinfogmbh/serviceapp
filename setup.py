1d75240ec8481bd362dc0aca6c31bb58b0ae8356
Richard Neumann <r.neumann@homeinfo.de>
2022-02-24 12:07:33 +0100

    Add Makefile and setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+"""Install script."""
+
+from setuptools import setup
+
+
+setup(
+    name='serviceapp',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'basex',
+        'peewee',
+        'peeweeplus',
+        'flask',
+        'werkzeug',
+        'wsgilib'
+    ],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info@homeinfo.de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r.neumann@homeinfo.de>',
+    packages=['tenant2tenant'],
+    license='GPLv3',
+    description='HIS microservice to handle tenant-to-tenant messages.'
+)

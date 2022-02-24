a6413fc732096221168b03a4a0d279abeaf316da
Richard Neumann <r.neumann@homeinfo.de>
2022-02-24 13:32:03 +0100

    Add authentication check

---
+++ serviceapp/authentication.py
@@ -0,0 +1,24 @@
+"""Authentication decorators."""
+
+from functools import wraps
+from typing import Any, Callable
+
+from wsgilib import JSONMessage
+
+from serviceapp.localproxies import get_current_user
+
+
+__all__ = ['authenticated']
+
+
+def authenticated(function: Callable) -> Callable:
+    """Decorates the given function with authentication."""
+
+    @wraps(function)
+    def wrapper(*args, **kwargs) -> Any:
+        if get_current_user().locked:
+            raise JSONMessage('Account locked.')
+
+        return function(*args, **kwargs)
+
+    return wrapper

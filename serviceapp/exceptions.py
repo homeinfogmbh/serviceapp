13f39ad6adeeaaf0250225b5e65610b05cba8485
Richard Neumann <r.neumann@homeinfo.de>
2022-03-01 13:10:15 +0100

    Add exceptions

---
+++ serviceapp/exceptions.py
@@ -0,0 +1,16 @@
+"""Common exceptions."""
+
+
+__all__ = ['InvalidPassword', 'NonceUsed', 'UserLocked']
+
+
+class InvalidPassword(Exception):
+    """Indicates that an invalid password was provided."""
+
+
+class NonceUsed(Exception):
+    """Indicates that a nonce has already been used."""
+
+
+class UserLocked(Exception):
+    """Indicates that the user is locked."""

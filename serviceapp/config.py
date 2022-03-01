13db3d525e79f93c84cfb7e782d991c73e5a6aea
Richard Neumann <r.neumann@homeinfo.de>
2022-03-01 13:10:01 +0100

    Add config

---
+++ serviceapp/config.py
@@ -0,0 +1,14 @@
+"""Configuration file loading."""
+
+from functools import partial
+from pathlib import Path
+
+from configlib import load_config
+
+
+__all__ = ['get_oauth2']
+
+
+OAUTH2_JSON = Path('/usr/local/etc/comcat.d/oauth2.json')
+
+get_oauth2 = partial(load_config, OAUTH2_JSON)

7f8c5c94aa98893de63c5353d83828d11f1b15d5
Richard Neumann <r.neumann@homeinfo.de>
2022-03-03 10:03:09 +0100

    Rewrite login
    TODO: Since this login method is nearly equal to the ComCat login method, outsource common parts into oauth2gen.

--- serviceapp/wsgi/login.py
+++ serviceapp/wsgi/login.py
@@ -1,21 +1,72 @@
 """User login."""
 
-from argon2.exceptions import VerifyMismatchError
-from flask import request
-from wsgilib import JSONMessage
+from functools import partial
+from typing import Union
+from uuid import UUID
 
-from serviceapp.orm import User
+from authlib.integrations.flask_oauth2 import AuthorizationServer
+from flask import request, Flask, Response
+
+from wsgilib import JSON, JSONMessage
+
+from serviceapp.exceptions import InvalidPassword, NonceUsed, UserLocked
+from serviceapp.oauth2 import FRAMEWORK
+from serviceapp.orm import AuthorizationNonce, User
 from serviceapp.pin import decode_pin
+from serviceapp.pwgen import genpw
 
 
-__all__ = ['ROUTES']
+__all__ = ['init_oauth_endpoints']
 
 
 INVALID_PIN = JSONMessage('Invalid PIN.')
 
 
-def login() -> JSONMessage:
-    """Logs in the user."""
+def init_oauth_endpoints(application: Flask) -> None:
+    """Adds OAuth endpoints to the respective application."""
+
+    server = FRAMEWORK.authorization_server(application)
+    application.route('/client', methods=['POST'])(register_client)
+    application.route(
+        '/authorize', methods=['POST'], endpoint='authorize_client'
+    )(
+        partial(authorize_client, server)
+    )
+    application.route('/oauth/token', methods=['POST'])(
+        server.create_token_response
+    )
+    application.route('/oauth/revoke', methods=['POST'])(server.revoke_token)
+    application.route('/oauth/introspect', methods=['POST'])(
+        server.introspect_token
+    )
+
+
+def authorize_client(
+        server: AuthorizationServer
+) -> Union[Response, JSONMessage]:
+    """Login is required since we need to know the current resource owner.
+    It can be done with a redirection to the login page, or a login
+    form on this authorization page.
+    """
+
+    if (nonce := request.form.get('nonce')) is None:
+        return JSONMessage('Missing nonce.', status=400)
+
+    try:
+        uuid = UUID(nonce)
+    except ValueError:
+        return JSONMessage('Invalid UUID.', status=400)
+
+    try:
+        user = AuthorizationNonce.use(uuid).user
+    except NonceUsed:
+        return JSONMessage('Invalid nonce.', status=400)
+
+    return server.create_authorization_response(grant_user=user)
+
+
+def register_client() -> JSON | JSONMessage:
+    """Registers a client."""
 
     if (pin := request.headers.get('pin')) is None:
         raise JSONMessage('No PIN provided.')
@@ -23,17 +74,23 @@ def login() -> JSONMessage:
     try:
         ident, passwd = decode_pin(pin)
     except ValueError:
-        raise INVALID_PIN from None
+        return INVALID_PIN
 
     try:
         user = User[ident]
     except User.DoesNotExist:
-        raise INVALID_PIN from None
+        return INVALID_PIN
 
     try:
-        user.password.verify(passwd)
-    except VerifyMismatchError:
-        raise INVALID_PIN from None
+        user.login(passwd)
+    except UserLocked:
+        return JSONMessage('This user is locked.', status=401)
+    except InvalidPassword:
+        return INVALID_PIN
 
-    ident, secret = Session.open(user)
-
+    transaction = FRAMEWORK.models.client.add(user, secret := genpw())
+    transaction.commit()
+    json = transaction.primary.to_json()
+    json['clientSecret'] = secret
+    json['authorizationNonce'] = AuthorizationNonce.add(user).uuid.hex
+    return JSON(json)
